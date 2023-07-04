29c233d6f0c31bdc63fcd10de386964a6d4217ca
Hassan Alhalabi <hassanalhalabi1997@outlook.com>
2023-07-04 11:07:15 +0300

    Init Project

---
+++ src/App.test.tsx
@@ -0,0 +1,29 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+
+import App from './App';
+import { expect } from 'vitest';
+
+describe('App Component Tests', () => {
+  it('Initial Render Main App', () => {
+    render(<App />);
+    const countButton = screen.getByRole('count-button')
+    expect(countButton).toHaveTextContent('0');
+  });
+
+  it('Counter Increament', () => {
+    render(<App />);
+    const countButton = screen.getByRole('count-button')
+    fireEvent(countButton,new MouseEvent('click', {
+      bubbles: true,
+      cancelable: true,
+    }))
+    fireEvent(countButton,new MouseEvent('click', {
+      bubbles: true,
+      cancelable: true,
+    }))
+  
+    expect(countButton).toHaveTextContent('2');
+  
+  });
+
+});
